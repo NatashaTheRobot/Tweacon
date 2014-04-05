da73920b9eb5e44b4d631bc0e841174a5af1dbea
Natasha Murashev <nmurashev@gmail.com>
2014-04-05 07:46:08 -0700

    initial commit

---
+++ Tweacon/NTRViewController.h
@@ -0,0 +1,13 @@
+//
+//  NTRViewController.h
+//  Tweacon
+//
+//  Created by Natasha Murashev on 4/5/14.
+//  Copyright (c) 2014 NatashaTheRobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTRViewController : UIViewController
+
+@end
---
+++ Tweacon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tweacon
+//
+//  Created by Natasha Murashev on 4/5/14.
+//  Copyright (c) 2014 NatashaTheRobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NTRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NTRAppDelegate class]));
+    }
+}
