9dd1ad18a0f0db92e4c8c7b1b3c426c391e712db
Natasha Murashev <nmurashev@gmail.com>
2014-04-20 17:24:27 -0700

    design adjustments

---
+++ Tweacon/UIImage+NTRImageEffects.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+NTRImageEffects.h
+//  Tweacon
+//
+//  Created by Natasha Murashev on 4/20/14.
+//  Copyright (c) 2014 NatashaTheRobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (NTRImageEffects)
+
+@end
---
+++ Tweacon/UIImage+NTRImageEffects.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+NTRImageEffects.m
+//  Tweacon
+//
+//  Created by Natasha Murashev on 4/20/14.
+//  Copyright (c) 2014 NatashaTheRobot. All rights reserved.
+//
+
+#import "UIImage+NTRImageEffects.h"
+
+@implementation UIImage (NTRImageEffects)
+
+@end
